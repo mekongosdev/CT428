621fc3515986baf27ab9642aefb758a942d09946
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-04-05 18:53:12 +0700

    Integration database connect

--- qly_nhansu.php
+++ qly_nhansu.php
@@ -1,4 +1,18 @@
-<?php require 'db_connect.php'; ?>
+<?php
+// Kết nối CSDL
+$db_host = "localhost"; // Ampps
+$db_user = "root"; // Ampps
+$db_pass = "mysql"; // Ampps
+$db_name = "ltweb"; // Ampps
+// $db_host = "172.30.35.70"; // Ltweb
+// $db_user = "user_c4"; // Ltweb
+// $db_pass = "puser_c4"; // Ltweb
+// $db_name = "db_c4"; // Ltweb
+$conn = mysql_connect($db_host,$db_user,$db_pass) or die(mysql_error());
+mysql_set_charset('utf8');
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+mysql_select_db($db_name) or die("mysql can not find");
+?>
 <!DOCTYPE html>
 <html lang="vi" dir="ltr">
   <head>
